7e7f147fb3c15fd30068fff90b3fe96ca4df2e8e
Strif-in <stefanodefina6@gmail.com>
2025-05-05 18:55:00 +0200

    Revert "initialisation backend"
    This reverts commit 64d9c766a4c06d47dd26c3c4f524f34632b4092a.
    
    .git/COMMIT_EDITMSG [unix] (18:55 05/05/2025)                                                                                     19,1 Botrecording @a  #

--- src/routes/messageRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createMessage } = require('../controllers/messageController');
-const router = express.Router();
-
-// Route POST /messages
-router.post('/', createMessage);
-
-module.exports = router;
